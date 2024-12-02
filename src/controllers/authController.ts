640982560b799bb7b54ba431df082fc9e778d13a
Md Naim Hossen <134233716+naiem9009@users.noreply.github.com>
2024-12-03 01:05:18 +0600

    Update authController.ts

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -79,7 +79,7 @@ class AuthController {
             res.cookie('token', token, {
                 httpOnly: true, 
                 secure: process.env.NODE_ENV === 'production', 
-                sameSite: 'strict',
+                sameSite: 'none',
                 maxAge: 3600000 
             });
 
